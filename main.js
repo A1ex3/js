1139626fae02de2f272d06b639cc8520b4909f3a
Alex <pakshin.2018@mail.ru>
2022-03-16 03:01:30 +0700

    pr7

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ function draw() {
 
 	let canvas = document.getElementById('canva');
 	let ctx = canvas.getContext('2d');
-	// координаты начала линии X,Y
+	// координаты начала первой линии X,Y
 	ctx.moveTo(200, 100);
 	// команда рисования линии с координатами конца линии
 	ctx.lineTo(500, 200);
 	ctx.strokeStyle = "pink"; //цвет линии
 	ctx.lineWidth = "10"; //толщина линии
+	ctx.stroke(); // обводка линии
+
+	// координаты начала второй линии X,Y
+	ctx.moveTo(300, 100);
 	ctx.lineTo(500, 50);
 	ctx.stroke();
 }
