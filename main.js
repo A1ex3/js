2e886142dfcb6d6d8ddf521578f7e616a3a76968
Alex <pakshin.2018@mail.ru>
2022-03-16 03:09:09 +0700

    pr20

--- main.js
+++ main.js
@@ -14,10 +14,14 @@ function draw() {
 	ctx.fill();
 
 
-	ctx.beginPath();// начало нового пути
-	ctx.lineWidth = 7; // толщина обводки
-	ctx.strokeStyle = "green"; // цвет обводки
-	ctx.moveTo(100, 150); // координаты начальной точки
-	ctx.bezierCurveTo(150, 50, 340, 10, 400, 200); // координаты точки искривления и конечной точки
-	ctx.stroke();
+	ctx.beginPath();
+	ctx.fillStyle = "red"; // цвет заливки
+	ctx.moveTo(75, 40);
+	ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+	ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+	ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+	ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+	ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+	ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+	ctx.fill();
 }
