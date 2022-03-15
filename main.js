6e654158396df6d72ec7446177941c6120d347e4
Alex <pakshin.2018@mail.ru>
2022-03-16 03:09:32 +0700

    pr21

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
 function draw() {
 
-	// ѕолучение элемента canvas, контекста и свойства Math.PI
-	let canvas = document.getElementById('canva');
-	let ctx = canvas.getContext('2d');
-	let pi = Math.PI;
+    // ѕолучение элемента canvas, контекста и свойства Math.PI
+    let canvas = document.getElementById('canva');
+    let ctx = canvas.getContext('2d');
+    let size = 75; // начальна€ позици€ X координаты
 
-	ctx.beginPath();
-	ctx.lineWidth = 7;
-	ctx.strokeStyle = "green";
-	ctx.fillStyle = "yellow";
-	ctx.arc(150, 150, 85, 0, 2 * pi, false);
-	ctx.stroke();
-	ctx.fill();
-
-
-	ctx.beginPath();
-	ctx.fillStyle = "red"; // цвет заливки
-	ctx.moveTo(75, 40);
-	ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-	ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-	ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-	ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-	ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-	ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
-	ctx.fill();
+    setInterval(function () {
+        ctx.beginPath();
+        ctx.clearRect(0, 0, 600, 300); // очистка холста
+        ctx.fillStyle = "pink"; // цвет заливки
+        ctx.rect(75, 75, size, size); // x, y, width, height
+        ctx.fill();
+        size++; // увеличение размера
+        if (size > 225) {
+            size = 75;
+        }
+    }, 60); // запуск каждые 60 миллисекунд  
 }
