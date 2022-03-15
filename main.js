916c0c8546e542db940598d8755ae094697c643c
Alex <pakshin.2018@mail.ru>
2022-03-16 03:02:52 +0700

    pr9

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ function draw() {
 	let canvas = document.getElementById('canva');
 	let ctx = canvas.getContext('2d');
 	ctx.beginPath();
+	ctx.beginPath();
 	ctx.strokeStyle = "red";
-	ctx.lineWidth = "20";
-	ctx.moveTo(100, 150);
-	ctx.lineTo(150, 75);
+	ctx.lineWidth = "30";
+	ctx.moveTo(100, 75);
+	ctx.lineTo(300, 75);
+	ctx.lineTo(300, 200);
 	ctx.lineCap = "round";
 	ctx.stroke();
 }
