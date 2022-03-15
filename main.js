c1a7f012a86c53ba7b70eafd2a378375f1bc2d46
Alex <pakshin.2018@mail.ru>
2022-03-16 03:00:28 +0700

    pr5

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ function draw() {
 
 	let canvas = document.getElementById('canva');
 	let ctx = canvas.getContext('2d');
-	ctx.stroke();
-	ctx.fillStyle = "pink"; //цвет заливки
-	ctx.beginPath();
-	ctx.lineWidth = 5; // толщина линии
-	// x,y,radius,startAngle,endAngle,anticlockwise
-	ctx.arc(200, 200, 75, 0, 2 * Math.PI, true);
-	ctx.stroke();
+	// координаты начала линии X,Y
+	ctx.moveTo(200, 100);
+	// команда рисования линии с координатами конца линии
+	ctx.lineTo(500, 200);
+	ctx.strokeStyle = "pink"; //цвет линии
+	ctx.lineWidth = "10"; //толщина линии
+	ctx.stroke(); // обводка линии   
 }
