059ad8f20de70e6ccaadc25c770a557ad8828848
Alex <pakshin.2018@mail.ru>
2022-06-15 02:32:23 +0700

    dom.6

--- dom/main.js
+++ dom/main.js
@@ -1,8 +1,3 @@
-function buttonClick() {
-	var input1 = document.getElementById('input1');
-	var input2 = document.getElementById('input2');
-	var input1Value = input1.value;
-	var input2Value = input2.value;
-	input1.value = input2Value;
-	input2.value = input1Value;
+function buttonClick(elem) {
+	elem.value = 'Новый текст кнопки';
 }
