96198a3c562f129e6e3b5ee502cf1ad1d0ea96d2
Alex <pakshin.2018@mail.ru>
2022-06-15 02:57:24 +0700

    dom.8

--- dom/main.js
+++ dom/main.js
@@ -1,3 +1,9 @@
-function buttonClick() {
-	document.getElementById('input').style.color = 'red';
+//Заблокирует элемент
+function button1Click() {
+	$('#input').prop("disabled", true);
+}
+
+//Отблокирует элемент
+function button2Click() {
+	$('#input').prop("disabled", false);
 }
