14c4f2a613d1f291384c3249ce42f90e914b4c27
Alex <pakshin.2018@mail.ru>
2022-06-15 02:31:03 +0700

    dom.5

--- dom/main.js
+++ dom/main.js
@@ -1,3 +1,8 @@
 function buttonClick() {
-	alert(Math.pow($('#input').val(),2));
+	var input1 = document.getElementById('input1');
+	var input2 = document.getElementById('input2');
+	var input1Value = input1.value;
+	var input2Value = input2.value;
+	input1.value = input2Value;
+	input2.value = input1Value;
 }
