15559a991bd5654a168e14421582d43d7b8acfab
Alex <pakshin.2018@mail.ru>
2022-06-15 03:07:14 +0700

    5

--- dom/main.js
+++ dom/main.js
@@ -1,9 +1,4 @@
-//Заблокирует элемент
-function button1Click() {
-	$('#input').prop("disabled", true);
-}
-
-//Отблокирует элемент
-function button2Click() {
-	$('#input').prop("disabled", false);
+function buttonClick()
+{
+    alert($('#input').val());
 }
