ca403afebda984e874558f579c42a5accfaba8b5
Alex <pakshin.2018@mail.ru>
2022-06-15 19:16:15 +0700

    23

--- dom/main.js
+++ dom/main.js
@@ -1,6 +1,10 @@
 function go() {
 	var test = document.getElementById('test');
 	var value = test.value;
-	var result = document.getElementById('result');
-	result.value = value * value;
+	if (!isNaN(value)) {
+		var result = document.getElementById('result');
+		result.value = value * value;
+	} else {
+		alert('¬ведено не число!')
+	}
 }
