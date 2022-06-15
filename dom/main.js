b5743de3aa6954626cfb00970033de514df7e651
Alex <pakshin.2018@mail.ru>
2022-06-15 18:52:37 +0700

    18

--- dom/main.js
+++ dom/main.js
@@ -1,9 +1,3 @@
-function block() {
-	$('#block').val('О, теперь на меня больше не нажать!').attr('disabled', true);
-	$('#unblock').css('display', 'block');
-}
-function unblock() {
-	$('#block').val('О, на меня снова можно нажимать').attr('disabled', false);
-	$('#unblock').css('display', 'none');
-
+function go(elem) {
+	elem.value = parseInt(elem.value) + 1;
 }
