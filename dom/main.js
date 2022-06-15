9715e711c16c5e4dfb793cbe7672562d3d9f16c2
Alex <pakshin.2018@mail.ru>
2022-06-15 18:39:46 +0700

    16

--- dom/main.js
+++ dom/main.js
@@ -1,7 +1,4 @@
-function hide() {
-    $('#test').css('display','none');
-}
-
-function show() {
-    $('#test').css('display', 'inline-block');
+function go() {
+    var styles = { 'width': '300px', 'height': '50px', 'color': 'red', 'borderRadius':'10px'}
+    $('#test').val('Ой, я поменял свой текст и css').css(styles);
 }
