b911098ca802651e4f1cb2e9bfe1421fcb30d029
Alex <pakshin.2018@mail.ru>
2022-06-15 19:17:50 +0700

    24

--- dom/main.js
+++ dom/main.js
@@ -1,10 +1,3 @@
 function go() {
-	var test = document.getElementById('test');
-	var value = test.value;
-	if (!isNaN(value)) {
-		var result = document.getElementById('result');
-		result.value = value * value;
-	} else {
-		alert('¬ведено не число!')
-	}
+	$('#test').attr('disabled', true).css('cursor', 'not-allowed');
 }
