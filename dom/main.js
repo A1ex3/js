a1385a6c3a58b0a7d7c410ea0b4230a66bacaa88
Alex <pakshin.2018@mail.ru>
2022-06-15 18:48:05 +0700

    17

--- dom/main.js
+++ dom/main.js
@@ -1,4 +1,9 @@
-function go() {
-    var styles = { 'width': '300px', 'height': '50px', 'color': 'red', 'borderRadius':'10px'}
-    $('#test').val('Ой, я поменял свой текст и css').css(styles);
+function block() {
+	$('#block').val('О, теперь на меня больше не нажать!').attr('disabled', true);
+	$('#unblock').css('display', 'block');
+}
+function unblock() {
+	$('#block').val('О, на меня снова можно нажимать').attr('disabled', false);
+	$('#unblock').css('display', 'none');
+
 }
